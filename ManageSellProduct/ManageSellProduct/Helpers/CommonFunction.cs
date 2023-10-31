5dceac09667d2779ecd3b468a157e5331a9fae51
Nghia 1 Lam Trong <lamtrong.nghia1@vietcredit.com.vn>
2023-10-31 19:29:01 +0700

    edit logic and remove bin, obj, .vs

--- ManageSellProduct/ManageSellProduct/Helpers/CommonFunction.cs
+++ ManageSellProduct/ManageSellProduct/Helpers/CommonFunction.cs
@@ -151,5 +151,170 @@ namespace ManageSellProduct.Helpers
         {
             return array.All(string.IsNullOrWhiteSpace);
         }
+
+        private static readonly string[] Digit = { "không ", "một ", "hai ", "ba ", "bốn ", "năm ", "sáu ", "bảy ", "tám ", "chín " };
+        private static readonly string[] Money = { "", "nghìn", "triệu", "tỷ", "nghìn", "triệu" };
+        private static readonly string[] Ext = { "linh ", "mốt ", "lăm ", "mươi ", "mười ", "trăm " };
+
+        public static string NumberToWordsVn(string num)
+        {
+            if (!long.TryParse(num, out long number))
+            {
+                return "Số tiền âm !";
+            }
+
+            long[] location = new long[6];
+            string result = "";
+            if (number < 0)
+            {
+                return "Số tiền âm !";
+            }
+
+            if (number == 0)
+            {
+                return "Không đồng !";
+            }
+
+            if (number > 8999999999999999)
+            {
+                return "Số quá lớn!";
+            }
+
+            location[5] = number / 1000000000000000;
+            number -= location[5] * 1000000000000000;
+
+            location[4] = number / 1000000000000;
+            number -= location[4] * 1000000000000;
+
+            location[3] = number / 1000000000;
+            number -= location[3] * 1000000000;
+
+            location[2] = number / 1000000;
+            location[1] = (number % 1000000) / 1000;
+            location[0] = number % 1000;
+
+
+            int times;
+            if (location[5] > 0)
+            {
+                times = 5;
+            }
+            else if (location[4] > 0)
+            {
+                times = 4;
+            }
+            else if (location[3] > 0)
+            {
+                times = 3;
+            }
+            else if (location[2] > 0)
+            {
+                times = 2;
+            }
+            else if (location[1] > 0)
+            {
+                times = 1;
+            }
+            else
+            {
+                times = 0;
+            }
+
+            for (int i = times; i >= 0; i--)
+            {
+                string tmp = ReadThreeNumber(location[i]);
+                if (i == times && tmp.StartsWith(Digit[0]))
+                {
+                    tmp = tmp.Replace(Digit[0], "").Replace(Ext[0], "").Replace(Ext[5], "");
+                }
+
+                result += tmp;
+
+                if (location[i] > 0)
+                {
+                    result += Money[i];
+                }
+
+                if (i > 0 && tmp.Length > 0)
+                {
+                    result += " ";
+                }
+            }
+
+            if (times > 3 && result.Trim().Split(' ').Length <= 5)
+            {
+                result += Money[3];
+            }
+
+            if (result.Substring(result.Length - 1) == ",")
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+
+            result = result.Substring(0, 1).ToUpper() + result.Substring(1);
+            return result + "đồng";
+        }
+
+        private static string ReadThreeNumber(long threeNumber)
+        {
+            string result = "";
+            long hundred = threeNumber / 100;
+            long dozen = (threeNumber % 100) / 10;
+            long unit = threeNumber % 10;
+
+            if (hundred == 0 && dozen == 0 && unit == 0)
+                return "";
+
+            result += Digit[hundred] + Ext[5];
+
+            if (dozen == 0 && unit != 0)
+                result += Ext[0];
+
+            if (dozen != 0 && dozen != 1)
+            {
+                result += Digit[dozen] + Ext[3];
+
+                if (dozen == 0 && unit != 0)
+                    result += Ext[0];
+            }
+
+            if (dozen == 1)
+                result += Ext[4];
+
+            switch (unit)
+            {
+                case 1:
+                    if (dozen != 0 && dozen != 1)
+                    {
+                        result += Ext[1];
+                    }
+                    else
+                    {
+                        result += Digit[unit];
+                    }
+
+                    break;
+                case 5:
+                    if (dozen == 0)
+                    {
+                        result += Digit[unit];
+                    }
+                    else
+                    {
+                        result += Ext[2];
+                    }
+
+                    break;
+                default:
+                    if (unit != 0)
+                    {
+                        result += Digit[unit];
+                    }
+
+                    break;
+            }
+
+            return result;
+        }
     }
 }
